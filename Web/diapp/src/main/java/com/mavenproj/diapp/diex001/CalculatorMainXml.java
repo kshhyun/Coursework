9827068b6858d466a587d7062a36910288290551
kshhyun <tjgus8288508@gmail.com>
2026-04-02 21:44:29 +0900

    CalculatorMainXml, calcSettings.xml 관련 파일 수정

--- Web/diapp/src/main/java/com/mavenproj/diapp/diex001/CalculatorMainXml.java
+++ Web/diapp/src/main/java/com/mavenproj/diapp/diex001/CalculatorMainXml.java
@@ -9,9 +9,11 @@ public class CalculatorMainXml {
 		//StudentScore student1 = new StudentScore(85,90, 100);
 		
 		ApplicationContext context = new ClassPathXmlApplicationContext("calcSettings.xml");
-		StudentScore student1 = (StudentScore) context.getBean(StudentScore.class);
+		//StudentScore student1 = (StudentScore) context.getBean(StudentScore.class);
+		StudentScore student5 = (StudentScore) context.getBean("student5");
 		
-		System.out.println(student1.toString());
+		// 생성자 이용 객체 초기치 설정에서, index 이용 값 설정 결과 확인 		
+		System.out.println(student5.toString());
 		
 		//1. 단순 산술 평균 계산
 		//ScoreCalculator avgCalc = new AverageCalculator(); //객체 생성
@@ -23,13 +25,18 @@ public class CalculatorMainXml {
 		rCard.printReport();
 		
 		//2. 가중치 산술 평균 계산
-		ScoreCalculator weightedCalc = new WeightedAvgCalculator(0.3, 0.3,0.4);
+		//ScoreCalculator weightedCalc = new WeightedAvgCalculator(0.3, 0.3,0.4);
+		//xml로부터 생성된 weightedCalc 객체를 가져와서 weightedCalc 객체 변수에 저장
+		ScoreCalculator weightedCalc = (ScoreCalculator)context.getBean("weightedCalc");
+		
 		//셋터이용 객체 설정
-		rCard.setCalc(weightedCalc);
+		//rCard.setCalc(weightedCalc);
+		ReportCard rCardWeight = (ReportCard) context.getBean("rCardWeight");
+		
 		//학생 점수는 위의 설정되어 있는 값을 활용
 		rCard.printReport(); 
 	
-		
+	
 		
 		
 		
--- Web/diapp/src/main/java/com/mavenproj/diapp/diex001/WeightedAvgCalculator.java
+++ Web/diapp/src/main/java/com/mavenproj/diapp/diex001/WeightedAvgCalculator.java
@@ -5,12 +5,17 @@ public class WeightedAvgCalculator implements ScoreCalculator{
 	private double engWeight;
 	private double mathWeight;
 	
-	//생성자를 이용하여 가중치 설정
-	public WeightedAvgCalculator(double korWeight, double engWeight, double mathWeight) {
-		this.korWeight = korWeight;
-		this.engWeight = engWeight;
-		this.mathWeight = mathWeight;
+	//인자가 없는 디폴트 생성자 설정
+	public WeightedAvgCalculator() {
+
 	}
+	
+	//생성자를 이용하여 가중치 설정
+		public WeightedAvgCalculator(double korWeight, double engWeight, double mathWeight) {
+			this.korWeight = korWeight;
+			this.engWeight = engWeight;
+			this.mathWeight = mathWeight;
+		}
 
 	
 	@Override
@@ -26,6 +31,32 @@ public class WeightedAvgCalculator implements ScoreCalculator{
 	@Override
 	public String getCalcMethod() {
 		return "가중치 산술 평균";
+	}
+	
+	//xml에서 property 태그를 실행하기 위해서는 setter() 메소드가 필요
+	//Source -> Generate Getters and Setters
+	public double getKorWeight() {
+		return korWeight;
+	}
+
+	public void setKorWeight(double korWeight) {
+		this.korWeight = korWeight;
+	}
+
+	public double getEngWeight() {
+		return engWeight;
+	}
+
+	public void setEngWeight(double engWeight) {
+		this.engWeight = engWeight;
+	}
+
+	public double getMathWeight() {
+		return mathWeight;
+	}
+
+	public void setMathWeight(double mathWeight) {
+		this.mathWeight = mathWeight;
 	}
 
 	
