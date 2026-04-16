3912de0ba2c0c408896d34706d4109595781e4ea
kshhyun <tjgus8288508@gmail.com>
2026-04-16 18:50:43 +0900

    spring 과제

--- Web/diapp/src/main/java/com/mavenproj/diapp/homework/FriendsMain.java
+++ Web/diapp/src/main/java/com/mavenproj/diapp/homework/FriendsMain.java
@@ -1,8 +1,27 @@
 package com.mavenproj.diapp.homework;
 
+import java.util.Map;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class FriendsMain {
 	public static void main(String[] args) {
-		//xml vkdlfdmf dlfrdjdhktj
-	}
+        // 1. XML 설정 파일을 읽어 컨테이너 생성
+		ApplicationContext context = new ClassPathXmlApplicationContext("friendsSettings.xml");
+        
+        // 2. FriendsMap 빈 가져오기
+        FriendsMap fMap = (FriendsMap) context.getBean("friendsMap");
+        Map<String, String> friendsList = fMap.getFriendsInfo();
+        
+        // 3. 데이터 출력 (실습 코드의 Map 출력 방식 참고)
+        System.out.println("========== 카카오 프렌즈 리스트 ==========");
+        for(Map.Entry<String, String> entry : friendsList.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+        System.out.println("=====================================");
+    }
 
 }
--- Web/diapp/src/main/java/com/mavenproj/diapp/homework/FriendsMap.java
+++ Web/diapp/src/main/java/com/mavenproj/diapp/homework/FriendsMap.java
@@ -1,5 +1,18 @@
 package com.mavenproj.diapp.homework;
 
-public class FriendsMap {
+import java.util.Map;
 
+public class FriendsMap {
+    // 멤버 변수 설정
+    private Map<String, String> friendsInfo;
+
+    // XML의 <property> 태그를 통해 주입받기 위한 Setter
+    public void setFriendsInfo(Map<String, String> friendsInfo) {
+        this.friendsInfo = friendsInfo;
+    }
+
+    // Getter
+    public Map<String, String> getFriendsInfo() {
+        return friendsInfo;
+    }
 }
