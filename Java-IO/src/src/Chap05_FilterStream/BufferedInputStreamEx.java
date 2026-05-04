fa7d1c439919a0a8ad84e5003ce8968c6d90fa64
kshhyun <tjgus8288508@gmail.com>
2026-05-04 22:47:04 +0900

    test.txt 추가  forest.jpg 수정

--- Java-IO/src/src/Chap05_FilterStream/BufferedInputStreamEx.java
+++ Java-IO/src/src/Chap05_FilterStream/BufferedInputStreamEx.java
@@ -8,14 +8,16 @@ public class BufferedInputStreamEx {
         long start = 0;
         long end = 0;
 
-        FileInputStream fis1 = new FileInputStream("forest.jpg");
+        //기본 기준점(Working Directory)은 항상 프로젝트의 최상위 폴더인 Java-IO
+        //따라서 /Users/hyun/.../Java-IO/까지는 다 생략 가능
+        FileInputStream fis1 = new FileInputStream("src/forest.jpg");
         start = System.currentTimeMillis();
         while(fis1.read()!=-1){}
         end = System.currentTimeMillis();
         System.out.println("사용하지 않았을 때: " + (end-start) + "ms");
         fis1.close();
 
-        FileInputStream fis2 = new FileInputStream("forest.jpg");
+        FileInputStream fis2 = new FileInputStream("src/forest.jpg");
         BufferedInputStream bis = new BufferedInputStream(fis2);
         start = System.currentTimeMillis();
         while (bis.read() != -1) {}
--- Java-IO/src/src/Chap05_FilterStream/OutputStreamWriterEx.java
+++ Java-IO/src/src/Chap05_FilterStream/OutputStreamWriterEx.java
@@ -5,7 +5,7 @@ import java.io.Writer;
 
 public class OutputStreamWriterEx {
     public static void main(String[] args) throws Exception {
-        FileOutputStream fos = new FileOutputStream("test.txt");
+        FileOutputStream fos = new FileOutputStream("src/test.txt");
         Writer writer = new java.io.OutputStreamWriter(fos);
 
         String data = "바이트 출력 스트림을 문자 출력 스트림으로 변환\n";
