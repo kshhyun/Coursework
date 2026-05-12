957224df148df357d547dc58dde4d2c97110e1e2
kshhyun <tjgus8288508@gmail.com>
2026-05-12 20:50:09 +0900

    add BoxPlot

--- BigData/practice2_visualization.R
+++ BigData/practice2_visualization.R
@@ -126,9 +126,14 @@ hist(mag, main="피지섬의 지진 발생 분포",
 lines(density(mag), col="red", lwd=2)
 
 # [BoxPlot]
-# 9-7 -------------------
-# 9-7-1 -------------------
-# 9-7-2 -------------------
+# 9-8 -------------------
+str(quakes)
+Q <- quantile(quakes$mag)
+value.upper <- Q[4] + 1.5*IQR(quakes$mag)
+value.lower <- Q[2] - 1.5*IQR(quakes$mag)
+# IQR(quakes$mag)
+boxplot(quakes$mag, main="피지섬의 지진 규모 분포", 
+        ylab="지진 강도", col='skyblue')
 
 # [Scatter Diagram]
 # 9-7 -------------------
