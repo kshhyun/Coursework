811a62ead425bba5599743f96207a489bba010db
kshhyun <tjgus8288508@gmail.com>
2026-05-12 21:01:56 +0900

    add scattor diagram

--- BigData/practice2_visualization.R
+++ BigData/practice2_visualization.R
@@ -137,7 +137,28 @@ boxplot(quakes$mag, main="피지섬의 지진 규모 분포",
 
 # [Scatter Diagram]
 # 9-9 -------------------
+#using the 'student_body.csv' file 
+setwd("~/Study/CourseWork/Coursework/BigData/CSV")
+DF <- read.csv("student_body.csv")
+str(DF)
+male <- subset(DF, Gender == "MALE")
+female <- subset(DF, Gender == "FEMALE")
+male
+female
 
+#하나의 화면에 여러개 시각화
+par(mfrow=c(1, 2))
+male_height <- male$Height
+male_weight <- male$Weight
+plot(male_height, male_weight, main="남성의 키와 몸무게 분포", 
+     xlab="키", ylab="몸무게", pch=21,
+     col="blue", bg="red", cex=1.5)
+
+female_height <- female$Height
+female_weight <- female$Weight
+plot(female_height, female_weight, main="여성의 키와 몸무게 분포", 
+     xlab="키", ylab="몸무게", pch=21,
+     col="blue", bg="green", cex=1.5)
 
 # [Plot]
 # 9-10 -------------------
