537bbd7af1782151112fd0d07482be8b148551d5
kshhyun <tjgus8288508@gmail.com>
2026-06-04 19:49:59 +0900

    add AroundAdvice class

---
+++ Web/aopEx/src/main/java/com/mavenproj/aopEx/ex002/AroundAdvice.java
@@ -0,0 +1,17 @@
+package com.mavenproj.aopEx.ex002;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.aop.aspectj.MethodInvocationProceedingJoinPoint;
+
+import com.mavenproj.aopEx.hw.ThrowingAdvice;
+
+public class AroundAdvice {
+	public Object aroundAdvice (ProceedingJoinPoint pjp) throws Throwable {
+		System.out.println("[AroundAdvice] 비즈니스 로직 전 수행");
+		
+		Object retObj = pjp.proceed();		
+		System.out.println("[AroundAdvice] 비즈니스 로직 후 수행");
+		
+		return retObj;
+	}
+}
